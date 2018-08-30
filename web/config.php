ac8238c007ba8a4dcccc025a5d3f4deaf3abb45a
SuperturtleOVO <42267115+SuperturtleOVO@users.noreply.github.com>
2018-08-30 19:21:09 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCMvCDIT_2ajNYu2VOsyPBz5AU-lgHe7m8');
-define('GJ_CODE', 'HK');
+define('GJ_CODE', 'US');
 define('SITE_NAME', '滑稽Tube');
 define('TITLENAME', '滑稽Tube');
 define('EN2DEKEY', 'superturtle2002');
