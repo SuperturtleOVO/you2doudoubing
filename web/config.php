831fde5b90a33af564b71a556e0ad94da057fb15
SuperturtleOVO <42267115+SuperturtleOVO@users.noreply.github.com>
2018-08-30 19:24:31 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,8 @@
 <?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyCMvCDIT_2ajNYu2VOsyPBz5AU-lgHe7m8');
-define('GJ_CODE', 'US');
+define('GJ_CODE', $gl);
 define('SITE_NAME', '滑稽Tube');
 define('TITLENAME', '滑稽Tube');
 define('EN2DEKEY', 'superturtle2002');
